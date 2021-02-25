fc6873f7b796198fa52245f12ac8dae853dd7921
vienvu <vien@vienvu.com>
2021-02-25 13:39:34 +0700

    make sample page

--- vue/src/components/page-header.vue
+++ vue/src/components/page-header.vue
@@ -3,29 +3,32 @@
  * A page title component - Renders the title with breadcrumbs
  */
 export default {
-	components: {},
-	props: {
-		title: {
-			type: String,
-			default: '',
-		},
-		items: {
-			type: Array,
-			default: () => {
-				return []
-			},
-		},
-	},
+  components: {},
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    items: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+  },
 }
 </script>
 
 <template>
-	<!-- start page title -->
-	<div class="row page-title">
-		<div class="col-md-12">
-			<b-breadcrumb :items="items" class="float-right mt-1"></b-breadcrumb>
-			<h4 class="mb-1 mt-0">{{ title }}</h4>
-		</div>
-	</div>
-	<!-- end page title -->
+  <!-- start page title -->
+  <div class="row page-title">
+    <div class="col-md-12">
+        <h4 class="mb-0 d-inline-block">
+          <slot name="actions"></slot>
+          {{ title }}</h4
+        >
+      <b-breadcrumb :items="items" class="float-right mt-1"></b-breadcrumb>
+    </div>
+  </div>
+  <!-- end page title -->
 </template>
--- vue/src/router/views/pages/order/all.vue
+++ vue/src/router/views/pages/order/all.vue
@@ -1,16 +1,137 @@
+<script>
+import draggable from 'vuedraggable'
+import appConfig from '@src/app.config'
+import Layout from '@layouts/main'
+import PageHeader from '@components/page-header'
+
+import Task from './board-task'
+import { tasks } from './data-taskboard'
+
+export default {
+  page: {
+    title: 'Project Tasks Board',
+    meta: [{ name: 'description', content: appConfig.description }],
+  },
+  components: { draggable, Layout, PageHeader, Task },
+  data() {
+    return {
+      todoTasks: [...tasks[0].items],
+      inProgressTasks: [...tasks[1].items],
+      reviewTasks: [...tasks[2].items],
+      doneTasks: [...tasks[3].items],
+      title: 'Orders',
+      items: [
+        {
+          text: 'Home',
+          href: '/',
+        },
+        {
+          text: 'Orders',
+          href: '/orders',
+        },
+      ],
+    }
+  },
+  computed: {
+    dragOptions() {
+      return {
+        group: 'tasks',
+      }
+    },
+  },
+}
+</script>
+
 <template>
   <Layout>
-    <div class="row page-title align-items-center">
-      <div class="col-sm-4 col-xl-6">
-        <h4 class="mb-1 mt-0">Orders</h4>
+    <PageHeader :title="title" :items="items">
+      <span slot="actions">
+        <router-link class="btn btn-primary ml-2 btn-sm" to="/new-order" >
+          <i class="uil-plus"></i>
+        </router-link>
+      </span>
+    </PageHeader>
+    <br />
+
+    <div class="row">
+      <div class="col-12">
+        <div class="board">
+          <!-- todo tasks -->
+          <div class="tasks border">
+            <h5 class="mt-0 task-header header-title"
+              >Todo <span class="font-size-13">({{ todoTasks.length }})</span>
+            </h5>
+
+            <div id="task-list-one" class="task-list-items">
+              <draggable v-model="todoTasks" v-bind="dragOptions">
+                <transition-group type="transition" :name="'flip-list'">
+                  <div v-for="task in todoTasks" :key="task.id">
+                    <Task :task="task" />
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
+          </div>
+          <!-- end - todo tasks -->
+
+          <!-- in progress tasks -->
+          <div class="tasks border">
+            <h5 class="mt-0 task-header header-title"
+              >In Progress
+              <span class="font-size-13">({{ inProgressTasks.length }})</span>
+            </h5>
+
+            <div id="task-list-one" class="task-list-items">
+              <draggable v-model="inProgressTasks" v-bind="dragOptions">
+                <transition-group type="transition" :name="'flip-list'">
+                  <div v-for="task in inProgressTasks" :key="task.id">
+                    <Task :task="task" />
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
+          </div>
+          <!-- end - in progress -->
+
+          <!-- review tasks -->
+          <div class="tasks border">
+            <h5 class="mt-0 task-header header-title"
+              >Review
+              <span class="font-size-13">({{ reviewTasks.length }})</span>
+            </h5>
+
+            <div id="task-list-one" class="task-list-items">
+              <draggable v-model="reviewTasks" v-bind="dragOptions">
+                <transition-group type="transition" :name="'flip-list'">
+                  <div v-for="task in reviewTasks" :key="task.id">
+                    <Task :task="task" />
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
+          </div>
+          <!-- end - review -->
+
+          <!-- done tasks -->
+          <div class="tasks border">
+            <h5 class="mt-0 task-header header-title"
+              >Done
+              <span class="font-size-13">({{ doneTasks.length }})</span>
+            </h5>
+
+            <div id="task-list-one" class="task-list-items">
+              <draggable v-model="doneTasks" v-bind="dragOptions">
+                <transition-group type="transition" :name="'flip-list'">
+                  <div v-for="task in doneTasks" :key="task.id">
+                    <Task :task="task" />
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
+          </div>
+          <!-- end - done -->
+        </div>
       </div>
     </div>
   </Layout>
 </template>
-<script>
-import Layout from '@layouts/main'
-
-export default {
-  components: { Layout },
-}
-</script>
