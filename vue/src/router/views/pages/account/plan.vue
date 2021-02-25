75b8169bcbc7f3ffa37a0a3aea7a9af21e76e7bb
vienvu <vien@vienvu.com>
2021-02-25 13:22:34 +0700

    update router new page

---
+++ vue/src/router/views/pages/account/plan.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <div class="row page-title align-items-center">
+      <div class="col-sm-4 col-xl-6">
+        <h4 class="mb-1 mt-0">Floor plan style</h4>
+      </div>
+    </div>
+  </Layout>
+</template>
+<script>
+import Layout from '@layouts/main'
+
+export default {
+  components: { Layout },
+}
+</script>
